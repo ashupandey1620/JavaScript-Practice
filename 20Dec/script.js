34f9088a86aad2a1e6cea93e862494585e7211dd
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-12-20 09:24:20 +0530

    Changes to be committed: 	modified:   20Dec/script.js

--- 20Dec/script.js
+++ 20Dec/script.js
@@ -3,3 +3,10 @@ divs[0].innerText = "Hello"
 divs[1].innerText = "I am "
 divs[2].innerText = "Hhere"
 divs[3].innerText = "only for you"
+
+
+let idx = 1;
+for(div of divs){
+    div.innerText = `new unique value ${idx}`
+    idx++
+}
