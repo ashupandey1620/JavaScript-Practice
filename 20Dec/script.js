64ecacb51b4418af38bf93bc33578cd5dc8e3efa
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-12-20 09:08:24 +0530

    Changes to be committed: 	modified:   20Dec/script.js

--- 20Dec/script.js
+++ 20Dec/script.js
@@ -1,3 +1,3 @@
 let hello = document.querySelector("h2")
-hello.innerText = hello.innerText + "From Youtube Academy of PSIT" //Concatenate
+hello.innerText = hello.innerText + " From Youtube Academy of PSIT" //Concatenate
 console.dir(hello.innerText)
